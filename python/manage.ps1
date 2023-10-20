fc3d6a8115a9c73b1ee30fe0a2aac7140ae1c52d
Michael Roberts <mjrobertsIT@gmail.com>
2023-10-20 15:39:50 +1100

    first commit

---
+++ python/manage.ps1
@@ -0,0 +1,119 @@
+# Python Scripts
+$Staging = @(
+[PSCustomObject]@{
+    tn = 'Area-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Area-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-09-30T13:10:26.786'
+},
+[PSCustomObject]@{
+    tn = 'Category-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Category-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-09-30T13:12:24.818'
+},
+[PSCustomObject]@{
+    tn = 'Period-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Period-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:22:06.156'
+},
+[PSCustomObject]@{
+    tn = 'Store-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Store-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:23:27.464'
+},
+[PSCustomObject]@{
+    tn = 'Unit-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Unit-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:23:27.353'
+},
+[PSCustomObject]@{
+    tn = 'Operation-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Operation-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:23:26.205'
+},
+[PSCustomObject]@{
+    tn = 'Ingredient-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Ingredient-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:24:48.535'
+},
+[PSCustomObject]@{
+    tn = 'Overhead-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Overhead-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:24:49.304'
+},
+[PSCustomObject]@{
+    tn = 'Recipe-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Recipe-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:24:50.336'
+},
+[PSCustomObject]@{
+    tn = 'RecipeIngredient-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/RecipeIngredient-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2023-01-28T06:49:24.173'
+},
+[PSCustomObject]@{
+    tn = 'Role-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Role-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:24:47.975'
+},
+[PSCustomObject]@{
+    tn = 'Supplier-lipwzvjrj5e5dgus2mtiogdk5i-staging'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Supplier-lipwzvjrj5e5dgus2mtiogdk5i-staging/stream/2022-07-16T14:24:49.184'
+})
+
+$Prod = @(
+[PSCustomObject]@{
+    tn = 'Area-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Area-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:34:51.173'
+},
+[PSCustomObject]@{
+    tn = 'Category-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Category-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:41.198'
+},
+[PSCustomObject]@{
+    tn = 'Period-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Period-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:31.283'
+},
+[PSCustomObject]@{
+    tn = 'Store-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Store-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:40.878'
+},
+[PSCustomObject]@{
+    tn = 'Unit-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Unit-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:30.319'
+},
+[PSCustomObject]@{
+    tn = 'Operation-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Operation-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:30.876'
+},
+[PSCustomObject]@{
+    tn = 'Ingredient-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Ingredient-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:39.522'
+},
+[PSCustomObject]@{
+    tn = 'Overhead-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Overhead-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:30.461'
+},
+[PSCustomObject]@{
+    tn = 'Recipe-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Recipe-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:31.722'
+},
+[PSCustomObject]@{
+    tn = 'RecipeIngredient-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/RecipeIngredient-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:31.140'
+},
+[PSCustomObject]@{
+    tn = 'Role-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Role-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:30.935'
+},
+[PSCustomObject]@{
+    tn = 'Supplier-n4gpkhbth5etrojcbgkmegsqfe-prod'
+    esarn = 'arn:aws:dynamodb:ap-southeast-2:633642160193:table/Supplier-n4gpkhbth5etrojcbgkmegsqfe-prod/stream/2023-03-18T05:36:31.054'
+})
+
+# DynamoDB Restore
+# DynamoDB console: restore to new table
+# Copy from new table to existing table
+pip install boto
+New-Item -Path Env: -Name AWS_DEFAULT_REGION -Value ap-southeast-2 
+New-Item -Path Env: -Name DISABLE_CREATION -Value true
+
+$i = 4
+For ($i=0; $i -lt 12; $i++) {
+$Source = $Staging[$i].tn
+$Destination = $Prod[$i].tn
+& "C:\Python310\python.exe" "C:\Users\Administrator\Documents\Python\dynamodb-copy-table\dynamodb-copy-table.py" $Source $Destination
+}
+
+# DynamoDB Table Backfill https://docs.amplify.aws/cli/graphql/troubleshooting/#backfill-opensearch-index-from-dynamodb-table
+pip3 install boto3
+$TablesStaging[3..6] | % {& "C:\Python310\python.exe" "C:\Users\Administrator\Documents\Python\ddb_to_es.py" --rn 'ap-southeast-2' --lf 'arn:aws:lambda:ap-southeast-2:633642160193:function:amplify-amplifya732575933-OpenSearchStreamingLambd-msduWFXhaMUY' --tn $_.tn --esarn $_.esarn}
+
