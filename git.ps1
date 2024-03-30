2e48cdc32b6cd8c7dccc17653cc63ad2a3f94abf
Michael Roberts <michael.roberts@hosposure.com.au>
2024-03-30 20:52:03 +1100

    added email template for reset-password

---
+++ git.ps1
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'added email template for reset-password'
+git push origin master
--- manage.ps1
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m 'added node script function for deleteUser'
-git push origin master
